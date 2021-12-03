bd217d146f917cee4a42d03347d8f3e3cd10bb06
Leshan Patterson <leshan0627@gmail.com>
2021-12-03 14:15:55 +0000

    fly over times using promises

---
+++ index2.js
@@ -0,0 +1,13 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+// see index.js for printPassTimes 
+// copy it from there, or better yet, moduralize and require it in both files
+
+// Call 
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
